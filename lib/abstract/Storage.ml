69df08c60fb13af083be70264a7d7b3366b2ed72
Estevan Castilho <tevo@riajuu.net>
2026-07-09 02:22:38 +0000

    Make storage operations monadic

--- lib/abstract/Storage.ml
+++ lib/abstract/Storage.ml
@@ -2,20 +2,20 @@ module type STORAGE = sig
   type connection
   type transaction
 
-  val connect: Concepts.Configuration.configuration -> connection
+  val connect: Concepts.Configuration.configuration -> (connection, Concepts.Condition.condition) result
 
   (** begin a transaction within `connection` *)
-  val start: connection -> transaction
+  val start: connection -> (transaction, Concepts.Condition.condition) result
 
   (** `commit` the current transaction *)
-  val commit: transaction -> unit
+  val commit: transaction -> (unit, Concepts.Condition.condition) result
 
   (** `abort` the current transaction *)
-  val abort: transaction -> unit
+  val abort: transaction -> (unit, Concepts.Condition.condition) result
 
   (** within a transaction, fetch a blob from disk *)
-  val get: transaction -> Concepts.Hash.hash -> Concepts.Representation.blob
+  val get: transaction -> Concepts.Hash.hash -> (Concepts.Representation.blob, Concepts.Condition.condition) result
 
   (** within a transaction, associate a hash with a blob on disk *)
-  val put: transaction -> Concepts.Hash.hash -> Concepts.Representation.blob -> unit
+  val put: transaction -> Concepts.Hash.hash -> Concepts.Representation.blob -> (unit, Concepts.Condition.condition) result
 end
