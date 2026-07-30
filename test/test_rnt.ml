391cfd787b3976580f5f9f125d2a26999d236c9f
Marcos Magueta <maguetamarcos@gmail.com>
2026-07-30 17:07:45 -0300

    dummy tests because I have to

--- test/test_rnt.ml
+++ test/test_rnt.ml
@@ -1,2 +1,6 @@
-let all_suites = Unit.Backend.Storage.suites () @ Unit.Backend.Lifecycle.suites ()
+let all_suites =
+  Unit.Concepts.Serialization.suites ()
+  @ Unit.Backend.Storage.suites ()
+  @ Unit.Backend.Lifecycle.suites ()
+
 let () = Alcotest.run "RNT" all_suites
