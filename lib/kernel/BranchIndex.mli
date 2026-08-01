d512411992a6a2b50fcdf03689129f70f530116f
Marcos Magueta <maguetamarcos@gmail.com>
2026-08-01 14:22:05 -0300

    Initialization with Branch State and Index

---
+++ lib/kernel/BranchIndex.mli
@@ -0,0 +1,28 @@
+type address = Concepts.Hash.hash
+type root = address
+type branch_name = string
+type branch_state_root = address
+
+val default_branch_name : branch_name
+
+module Make (Store : Abstract.Storage.STORAGE) : sig
+  type tree
+
+  val empty : tree
+  val hash_of : tree -> root
+  val persist : Store.transaction -> tree -> (root, Concepts.Condition.condition) result
+  val find : Store.transaction -> root -> (tree option, Concepts.Condition.condition) result
+
+  val find_branch :
+    Store.transaction ->
+    branch_name ->
+    tree ->
+    (branch_state_root option, Concepts.Condition.condition) result
+
+  val put_branch :
+    Store.transaction ->
+    branch_name ->
+    branch_state_root ->
+    tree ->
+    (tree, Concepts.Condition.condition) result
+end
