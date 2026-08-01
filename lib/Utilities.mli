9233478bd408280f28cea76a05a2a337966322ab
Estevan Castilho <tevo@riajuu.net>
2026-08-01 03:09:03 +0000

    Generic `sequence`, finger tree `map2`/`zip`

--- lib/Utilities.mli
+++ lib/Utilities.mli
@@ -1,5 +1,15 @@
 module Result : sig
   val ( let* ) : ('a, 'e) result -> ('a -> ('b, 'e) result) -> ('b, 'e) result
   val fmap : ('a -> ('b, 'e) result) -> ('a, 'e) result -> ('b, 'e) result
+end
+
+module List : sig
   val sequence : ('a, 'b) result list -> ('a list, 'b) result
+end
+
+module FingerTree : sig
+  val map2 : ('a -> 'b -> 'c) -> 'a BatFingerTree.t -> 'b BatFingerTree.t -> 'c BatFingerTree.t
+  val zip : 'a BatFingerTree.t -> 'b BatFingerTree.t -> ('a * 'b) BatFingerTree.t
+
+  val sequence : ('a, 'b) result BatFingerTree.t -> ('a BatFingerTree.t, 'b) result
 end
