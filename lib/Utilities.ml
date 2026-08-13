d465e42a991f8a22f1bbb803adc1756311d2278e
Estevan Castilho <tevo@riajuu.net>
2026-08-13 02:35:36 +0000

    Atomic swap (and monadic swap)

--- lib/Utilities.ml
+++ lib/Utilities.ml
@@ -10,3 +10,20 @@ module Result = struct
                       ms
                       (Ok [])
 end
+
+module Atomic = struct
+  let rec swap atom f =
+    let v = Atomic.get atom in
+    let v' = f v in
+    if Atomic.compare_and_set atom v v'
+    then atom
+    else swap atom f
+
+  let rec mswap atom f =
+    let open Result in
+    let v = Atomic.get atom in
+    let* v' = f v in
+    if Atomic.compare_and_set atom v v'
+    then Ok atom
+    else mswap atom f
+end
