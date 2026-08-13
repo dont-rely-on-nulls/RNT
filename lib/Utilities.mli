afbab0968340abcec044d9ac70c4a3ec0756933e
Estevan Castilho <tevo@riajuu.net>
2026-08-13 02:35:36 +0000

    Atomic swap (and monadic swap)

--- lib/Utilities.mli
+++ lib/Utilities.mli
@@ -3,3 +3,9 @@ module Result : sig
   val fmap : ('a -> ('b, 'e) result) -> ('a, 'e) result -> ('b, 'e) result
   val sequence : ('a, 'b) result list -> ('a list, 'b) result
 end
+
+module Atomic : sig
+  val swap : 'a Atomic.t -> ('a -> 'a) -> 'a Atomic.t
+  (* This is silly *)
+  val mswap : 'a Atomic.t -> ('a -> ('a, 'b) result) -> ('a Atomic.t, 'b) result
+end
