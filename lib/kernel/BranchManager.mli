72fe5859dc5c4c98da3764092668b8e02a472697
Estevan Castilho <tevo@riajuu.net>
2026-08-20 00:00:36 +0000

    BranchManager sketch

--- lib/kernel/BranchManager.mli
+++ lib/kernel/BranchManager.mli
@@ -3,8 +3,8 @@ type branch = string            (* FIXME *)
 module Make (S : Abstract.Storage.STORAGE) : sig
   type manager
 
-  val initialize : S.connection -> Concepts.Hash.hash -> (manager, Concepts.Condition.condition) result
-  val fetch : manager -> string -> (Concepts.Branch.t option, Concepts.Condition.condition) result
+  val initialize : S.connection -> string -> (manager, Concepts.Condition.condition) result
+  val fetch : manager -> string -> (branch option, Concepts.Condition.condition) result
   val update : manager -> string -> (branch -> branch) -> (manager, Concepts.Condition.condition) result
 
   (* TODO: fork, join *)
