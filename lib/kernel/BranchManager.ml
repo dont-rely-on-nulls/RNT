465f3378daea5a6f0d44f3729c3551a9cda78ed9
Estevan Castilho <tevo@riajuu.net>
2026-08-20 00:00:36 +0000

    BranchManager sketch

--- lib/kernel/BranchManager.ml
+++ lib/kernel/BranchManager.ml
@@ -2,28 +2,52 @@ module BranchValue = Merkle.StringKey (* FIXME *)
 
 type branch = string
 
+module Error = struct
+  open Concepts.Condition
+
+  let invalid_root head = condition "invalid-root" "The root hash for the database state is missing from the backend. Either your storage is corrupted, or this is a bug in RNT!"
+                            ("hash" |=| (Concepts.Value.String (Concepts.Hash.to_hum_string head)))
+end
+
 module Make (S : Abstract.Storage.STORAGE) = struct
   module I = Merkle.Interface (S) (Merkle.StringKey) (BranchValue)
   module SI = Storage.Make (S)
 
-  type manager = { storage: S.connection; head: I.node Atomic.t }
+  type manager = { storage: S.connection;
+                   label : string;
+                   head: I.node Atomic.t }
 
-  let initialize storage addr =
+  let root_for tx label =
+    let open Utilities.Result in
+    let* label = S.get tx (S.Label label) in
+    let label = Option.map Concepts.Hash.hash_of_blob label in
+    Ok label
+
+  let persist_root tx label new_branch = failwith "TODO"
+
+  let initialize storage label =
     let open Utilities.Result in
     SI.with_transaction storage (fun tx ->
-        let* head = I.find tx addr in
-        Ok { storage; head = Option.value head ~default:I.empty |> Atomic.make })
+        let* addr = root_for tx label in
+        match addr with
+        | None -> failwith "TODO"
+        | Some addr ->
+           let* head = I.find tx addr in
+           let* head = Option.to_result ~none:(Error.invalid_root addr) head in
+           Ok { storage; label; head = Atomic.make head })
 
-  let fetch { storage; head } branch_name =
+  let fetch { storage; head; _ } branch_name =
     SI.with_transaction storage (fun tx ->
         let head = Atomic.get head in
         I.lookup tx branch_name head)
 
-  let update ({ storage; head } as manager) branch_name f =
+  let update ({ storage; head; label } as manager) branch_name f =
     let open Utilities.Result in
-    SI.with_transaction storage (fun tx ->
-        let* new_head = Utilities.Atomic.mswap head (fun head ->
-                            let* branch = I.lookup tx branch_name head in
-                            f branch) in
-        Ok { manager with head = new_head })
+    let* _ = SI.with_transaction storage (fun tx ->
+                 Utilities.Atomic.mswap head (fun head ->
+                     let* branch = I.lookup tx branch_name head in
+                     let new_branch = f branch in
+                     let* () = persist_root tx label new_branch in
+                     new_branch)) in
+    Ok ()
 end
--- lib/kernel/Initialization.mli
+++
@@ -1,9 +0,0 @@
-type address = Concepts.Hash.hash
-type branch_index_root = address
-
-val fixed_label : string
-val fixed_label_address : address
-
-module Make (Store : Abstract.Storage.STORAGE) : sig
-  val initialize : Store.connection -> (branch_index_root, Concepts.Condition.condition) result
-end
